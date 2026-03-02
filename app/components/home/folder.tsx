a2210415aa82ebdfd90d070ed1f1ecdd37949e1c
redraze <coross3@outlook.com>
2026-03-02 15:38:49 -0800

    replace usestate instances in folders with new folder store

--- app/components/home/folder.tsx
+++ app/components/home/folder.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
+import { useFolderStore } from "~/lib/folderStore";
 import File from "./file";
 import type { FolderType } from "~/lib/fileStructure";
 
 export default function Folder({ foldername, files }: FolderType) {
+    const folderState = useFolderStore((state) => state.folderState);
+    const toggleFolder = useFolderStore((state) => state.toggleFolder);
+    
     const isHidden = foldername[0] === ".";
-
-    const [active, setActive] = useState(false);
-    const toggleActive = () => setActive(!active);
 
     return (
         <div className="flex flex-col">
-            <div onClick={toggleActive} className="flex items-center py-[1.5px] pl-3">
+            <div onClick={() => toggleFolder(foldername)} className="flex items-center py-[1.5px] pl-3">
                 {
-                    active
+                    folderState[foldername]
                         ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffffd0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><path d="M6 9l6 6 6-6"/></svg>
                         : <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffffd0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><path d="M9 18l6-6-6-6"/></svg>
                 }
                 <span className={isHidden ? "hiddenText pl-1": "normalText pl-1"}>{foldername}</span>
             </div>
 
-            <div className={active ? "" : "hidden"}>
+            <div className={folderState[foldername] ? "" : "hidden"}>
                 {
                     files.map((content, idx) => (
                         <File 
