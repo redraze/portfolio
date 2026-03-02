b0168357b8fcf43e8d34a9849b83fb869c37347c
redraze <coross3@outlook.com>
2026-03-02 15:38:11 -0800

    add orbit controls to scene

--- app/content/defaultContent.tsx
+++ app/content/defaultContent.tsx
@@ -2,12 +2,14 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "~/components/content/loader";
 import FilingCabinet from "~/components/content/filingCabinet";
+import { OrbitControls } from "@react-three/drei";
 
 export default function DefaultContent() {
     return (
         <div className="flex grow">
             <Suspense fallback={<Loader />}>
                 <Canvas camera={{ position: [4, 4, -2] }}>
+                    <OrbitControls />
                     <ambientLight intensity={Math.PI / 2} />
                     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                     <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
