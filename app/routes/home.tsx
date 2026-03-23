3a26e8f6c1a36c22061eb90edbeb115a44855f3c
redraze <coross3@outlook.com>
2026-03-23 11:25:01 -0700

    add an event listener for a keyboard shortcut to clear selected content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useContentStore } from "~/lib/contentStore";
 import type { Route } from "./+types/home";
 import TopBar from "~/components/home/topbar";
 import LeftBar from "~/components/home/leftbar";
@@ -13,6 +15,21 @@ export function meta({}: Route.MetaArgs) {
 };
 
 export default function Home() {
+  const clearContent = useContentStore((state) => state.clearContent);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    const key = e.key;
+    if (key === 'Escape') clearContent();
+  };
+
+  useEffect(() => {
+    if (!document) return;
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
   return (
     <main className="flex flex-col min-h-screen">
       <TopBar />
