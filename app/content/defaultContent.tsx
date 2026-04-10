5a9f8361e1f5b35b98b97b2e0c6e23322933c7d1
redraze <coross3@outlook.com>
2026-04-09 21:18:41 -0700

    fill out the volunteer content

--- app/content/defaultContent.tsx
+++ app/content/defaultContent.tsx
@@ -33,7 +33,7 @@ export default function DefaultContent() {
                 { content?.component && 
                     <div className="px-8 pt-14 overflow-auto absolute h-[100%] w-[100%]">
                         { content.component }
-                        <div className="h-[85vh]"></div>
+                        <div className="h-[75vh]"></div>
                     </div>
                 }
             </div>
--- app/content/volunteerExperience.tsx
+++ app/content/volunteerExperience.tsx
@@ -1,17 +1,12 @@
+import Header from "~/components/markdown/header";
+import ListElement from "~/components/markdown/listElement";
+import Text from "~/components/markdown/text";
+import { useContentStore } from "~/lib/contentStore";
+import { fileNameKeys, fileSystemMap, folderNameKeys } from "~/lib/fileStructure";
+import { useFolderStore } from "~/lib/folderStore";
+
 export default function VolunteerExperience() {
     /**
-     * 
-     * ==========
-     * keelworks
-     * ==========
-     * 
-     * keelworks is an org dedicated to helping aspiring developers gain experience.
-     * 
-     * 
-     * one of the projects that was being build there was a water utility management 
-     * system for well-water dependent communities (sound familiar?)
-     * 
-     * 
      * I decided to volunteed some time to Keelworks for two reasons:
      * - make new connections with other developers
      * - bring some expertise/fresh ideas to their water utility app
@@ -26,5 +21,36 @@ export default function VolunteerExperience() {
      * 
      * 
      */
-    return <span>volunteer</span>
-}
+    const setContent = useContentStore((state) => state.setContent);
+    const folderState = useFolderStore((state) => state.folderState);
+    const setFolderState = useFolderStore((state) => state.setFolderState);
+
+    const openFreelanceContent = () => {
+        const content = fileSystemMap[fileNameKeys.FREELANCE];
+        setContent(content);
+
+        setFolderState({
+            ...folderState,
+            [folderNameKeys.PROJECTS]: true,
+        });
+    };
+
+    return (<>
+        <Header size="large">Volunteer Work</Header>
+        <Text>Early 2025 I volunteered some time at <a href="https://keelworks.org/">Keelworks</a>, an organization dedicated to helping software developers gain experience.</Text>
+
+        <Header size="medium">Why?</Header>
+        <Text>There were two reasons I decided to volunteer at Keelworks:</Text>
+        <ListElement>Keelworks was working on a water utility management web application, and I wanted to offer some expertise and fresh ideas since I had <a onClick={openFreelanceContent}>built something similar while freelancing</a></ListElement>
+        <ListElement>I remember how difficult is was starting out and not being to even land an interview, and I wanted to offer what I knew to other developers</ListElement>
+
+        <Header size="medium">The Project</Header>
+        <Text>Their frontend team was blocked because they weren't getting the APIs they needed for their login/signup flow, so I filled in as a backend engineer.</Text>
+        <Text>I cleaned up thier Node + Express server configuration, updated Postgres db schemas, and wrote new models and controllers to handle JWT authentication.</Text>
+        <Text>Within a week I was serving new requests and had their team completely unblocked.</Text>
+
+        <Header size="medium">The Results</Header>
+        <Text>I only volunteered for a few months at Keelworks, but I really enjoyed working with the people there.</Text>
+        <Text>It was also really great to see just how far I had come as a software engineer! A year or two prior and I really would have struggled to understand and solve the team's issues and drive progress towards the team's goals.</Text>
+    </>);
+};
