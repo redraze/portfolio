f2ce144d41c4f557f1a85ac13a1441936e4aeedb
redraze <coross3@outlook.com>
2026-04-20 15:16:52 -0700

    add info about neverending freelance experience

--- app/content/freelanceExperience.tsx
+++ app/content/freelanceExperience.tsx
@@ -1,28 +1,17 @@
 import Header from "~/components/markdown/header";
 import ListElement from "~/components/markdown/listElement";
 import Text from "~/components/markdown/text";
+import Tooltip from "~/components/markdown/tooltip";
+import jira from 'public/jira.gif';
+import Link from "~/components/markdown/link";
 
 export default function FreelanceExperience() {
-    /**
-     * =============
-     * NEVERENDING
-     * =============
-     * 
-     * managed three terams comprised of ~15 incoming inters/juniors
-     * first time managing other developers, so many meetings!
-     * 
-     * 
-     * favorite project: built a CI/CD pipeline that invalidates and pushes updates to a CDN
-     * favorite project because it was first time I got to deploy large-scale (60k+ users) system architecture
-     * AWS S3, EC2
-     * 
-     */
     return (
         <>
             <Header size="large">Freelance Experience</Header>
-            <Text>Originally I got into web development to work on cool projects that will simplify people's lives or work, so I was really excited when I landed my first gig as a freelancer!</Text>
+            <Text>Originally I got into web development to work on cool projects that will simplify people's lives or work, so I was really excited when I landed my first gigs as a freelancer!</Text>
 
-            <section about="boon water">
+            <section>
                 <Header size="medium">Boon Water</Header>
                 <Text>Boon Water is a tiny company of neighbors that came together to manage their well water system themselves.</Text>
                 <Text>In the past I had worked with them to build and update their old-school Excel spreadsheets that were used for tracking water usage. Since I already knew what they wanted, I tried to make something familiar and straightforward.</Text>
@@ -43,9 +32,22 @@ export default function FreelanceExperience() {
                 <ListElement>Payments - enable neighbors to pay their bills online instead of mailing checks</ListElement>
             </section>
 
-            <section about="never ending">
+            <section>
                 <Header size="medium">NeverEnding</Header>
-                <Text></Text>
+                <Text>NeverEnding is a creative platform for storytellers and gamers, and I was brought in to finish integrating a pre-built Unity project that allowes users to design and 3D charaters for live-streaming.</Text>
+                <Text>
+                    On top of my assigned dev duties, I quickly became the manager and first line of support for three teams of interns.
+                    This was the first time I had worked as a part of a dev team so I was really overwhlemed at first, 
+                    <Tooltip tooltip={<img src={jira} />} >
+                        especially with all the meetings!
+                    </Tooltip>
+                </Text>
+
+                <Header size="small">Projects</Header>
+                <Text>My main task of integrating the Unity project consisted of designing React components and a <Link href="https://react-unity-webgl.dev/">React-Unity-WebGL wrapper</Link>, which ended up not taking me very long to finish, so I went looking for other stuff to work on.</Text>
+                <Text>Up to this point I had super limited experience working with cloud provider services, which is why I took the intitiative to:</Text>
+                <ListElement>Build a Github Actions pipeline that responds to updates in the Unity project files, invalidating and pushing updates to the AWS CloudFront CDN</ListElement>
+                <ListElement>Configure a new AWS service cluster (S3, EC2, Route53, Elastic IP) for handling API requests from character creation services</ListElement>
             </section>
         </>
     )
