9ad90616bed3d22cdf3600c6f9415fdd66f7979a
redraze <coross3@outlook.com>
2026-04-24 14:03:17 -0700

    add starter info for sprqs content

--- app/content/sprqsExperience.tsx
+++ app/content/sprqsExperience.tsx
@@ -1,10 +1,13 @@
+import Header from "~/components/markdown/header";
+import Link from "~/components/markdown/link";
+import Text from "~/components/markdown/text";
+
 export default function SprqsExperience() {
-    /**
-     * 
-     * Taking everything I've learned throughout my self -taught journey to make THE PERFECT app!
-     * 
-     * 
-     * 
-     */
-    return <span>sprqs</span>
-}
+    return (
+        <>
+            <Header size="large">Sprqs</Header>
+            <Text>Founding engineer for <Link href="https://sprqs.com/">✨SPRQS✨</Link>, the up-and-coming, <span className="rainbow-text">HOT AS HECK</span>, anonymous positivity social messaging app.</Text>
+            <Text>Sprqs is being built to provide a safe space for friends to send each other fun, anonymous mesages to brighten each other's days.</Text>
+        </>
+    );
+};
